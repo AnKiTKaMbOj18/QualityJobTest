b6f8d6d802b0052425a89860ef0d9f023c5a2855
ankitkamboj18 <ankitkamboj124@gmail.com>
2018-09-13 11:09:38 +0530

    initial project setup

---
+++ src/main/java/com/QualityJobTest/QualityJobTestApplication.java
@@ -0,0 +1,12 @@
+package com.QualityJobTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityJobTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityJobTestApplication.class, args);
+	}
+}
